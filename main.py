1df61557e314fedf385b903d98585a101a3bceb4
kaoulis <kyriakoschiotis@gmail.com>
2025-03-27 01:26:06 +0200

    in progress

--- main.py
+++ main.py
@@ -1,91 +1,42 @@
 import pandas as pd
 import streamlit as st
-from reports import EeconeReport, DataPreviewReport, EvidentlyReport, LifetimeExplorationReport
+from reports import *
 from static.styles import apply_css
-from evidently.metric_preset import DataDriftPreset, DataQualityPreset, TargetDriftPreset
 import warnings
 warnings.filterwarnings("ignore", category=FutureWarning, message="'H' is deprecated and will be removed in a future version, please use 'h' instead.")
 
 
-st.set_page_config(page_title="EECONE", page_icon="static/eecone_icon.png", layout="wide")
-apply_css(sidebar_width=420)
-reports: dict[str, EeconeReport] = {
-    # '🔍 Data Preview': DataPreviewReport(),
-    '📊 Data Quality': EvidentlyReport(DataQualityPreset()),
-    '👁️ Data Drift': EvidentlyReport(DataDriftPreset()),
-    '🎯 Target Drift': EvidentlyReport(TargetDriftPreset()),
+st.set_page_config(page_title="Drift Detection", page_icon="static/icsa_logo.png", layout="wide")
+apply_css(sidebar_width=440)
+reports: dict[str, Report] = {
+    '🔍 Data Preview': DataPreviewReport(),
+    '📊 Data Quality': DataQualityReport(),
+    '👁️ Data Drift': DataDriftReport(),
+    '🎯 Target Drift': TargetDriftReport(),
     '⏳ Lifetime Exploration': LifetimeExplorationReport(),
 }
 
 
 def main():
-    if st.session_state.get("data") is None:
-        df = get_dummy_data()
-    else:
-        df = st.session_state["data"]
 
     with st.sidebar:
         st.text("")
-        _, icon, title = st.columns([0.01, 0.4, 0.63])
-        icon.markdown("<a href='https://www.eecone.com/eecone/home/'><img src='app/static/eecone_icon.png' style='width:95%;'></a>", unsafe_allow_html=True)
-        title.markdown("<h1 style='color: #2EAA53;'>Eco-design Tool Advanced Reliability Lifetime Exploration</h1>", unsafe_allow_html=True)
-
-        if st.button(st.session_state.get('csv_file_name', 'dummy_data.csv'), icon='✏️'):
-            upload_data()
+        icon, title = st.columns([0.3, 0.7])
+        icon.markdown("<a href='https://icsa.hua.gr/'><img src='app/static/icsa_logo.png' style='width:95%;'></a>", unsafe_allow_html=True)
+        title.markdown("<h1>Intelligent Computer Systems & Applications Drift Detection</h1>", unsafe_allow_html=True)
+        st.divider()
         
         with st.form("my_form"):
             explore_item = st.selectbox('Report', list(reports.keys()))
-            st.selectbox('Datetime column', df.columns, key="datetime_column")
-            st.date_input('Reference Period', key="ref_period")
-            st.date_input("Current Period", key="cur_period")
+            ref_data = st.file_uploader("Reference Data")
+            cur_data = st.file_uploader("Current Data")
             submitted = st.form_submit_button("Create Report")
 
-    if submitted or explore_item == "📊 Data Quality":
-        df_ref = df[(df[st.session_state['datetime_column']] >= pd.to_datetime(st.session_state['ref_period'][0])) & (df["timestamp"] <= pd.to_datetime(st.session_state['ref_period'][1]))]
-        df_prod = df[(df[st.session_state['datetime_column']] >= pd.to_datetime(st.session_state['cur_period'][0])) & (df["timestamp"] <= pd.to_datetime(st.session_state['cur_period'][1]))]
+    if submitted:
         with st.spinner("Wait for it...", show_time=True):
-            reports[explore_item].create_report(df_ref, df_prod)
+            reports[explore_item].create_report(ref_data, cur_data)
 
     print("Done!")
-
-
-
-def get_dummy_data():
-    df = pd.read_csv("data/dummy_data.csv", delimiter=",")
-    df.convert_dtypes()
-    df['timestamp'] = pd.to_datetime(df['timestamp'])
-    df['target'] = df['mill_kw']
-    st.session_state["data"] = df
-    st.session_state['datetime_column'] = "timestamp"
-    st.session_state['ref_period'] = (df["timestamp"].min(), df["timestamp"].min() + pd.Timedelta(days=1))
-    st.session_state['cur_period'] = (df["timestamp"].max() - pd.Timedelta(days=1), df["timestamp"].max())
-
-    return df
-
-
-@st.dialog("Upload your data")
-def upload_data():
-    uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
-    if uploaded_file is not None:
-        df = pd.read_csv(uploaded_file, delimiter=",")
-        df.convert_dtypes()
-        df['timestamp'] = pd.to_datetime(df['timestamp'])
-
-        st.session_state['csv_file_name'] = uploaded_file.name
-        st.session_state["data"] = df
-        st.session_state['datetime_column'] = try_to_find_datetime_column(df)
-        st.session_state['ref_period'] = (df["timestamp"].min(), df["timestamp"].min() + pd.Timedelta(days=1))
-        st.session_state['cur_period'] = (df["timestamp"].max() - pd.Timedelta(days=1), df["timestamp"].max())
-        st.toast("Data uploaded successfully!", icon='🎉')
-        st.rerun()
-
-
-def try_to_find_datetime_column(df: pd.DataFrame) -> str:
-    for col in df.columns:
-        if df[col].dtype == "datetime64[ns]":
-            print(f"Found datetime column: {col}")
-            return col
-    return df.columns[1]
 
 
 if __name__ == "__main__":
--- reports.py
+++ reports.py
@@ -1,28 +1,15 @@
 from abc import ABC, abstractmethod
 import pandas as pd
 import streamlit as st
-import streamlit.components.v1 as components
-from evidently.report import Report
-from evidently.metric_preset.metric_preset import MetricPreset
 
 
-class EeconeReport(ABC):
+class Report(ABC):
     @abstractmethod
     def create_report(self, df_ref, df_prod):
         pass
 
 
-class EvidentlyReport(EeconeReport):
-    def __init__(self, report: MetricPreset):
-        self.report = report
-    
-    def create_report(self, df_ref, df_prod):
-        report = Report([self.report])
-        report.run(reference_data=df_ref, current_data=df_prod)
-        components.html(report._repr_html_(), height=920, scrolling=True)
-
-
-class DataPreviewReport(EeconeReport):
+class DataPreviewReport(Report):
     def create_report(self, df_ref: pd.DataFrame, df_prod: pd.DataFrame):
         st.write("Reference data:")
         st.write(df_ref.head(100))
@@ -30,8 +17,23 @@ class DataPreviewReport(EeconeReport):
         st.write(df_prod.head(100))
 
 
-class LifetimeExplorationReport(EeconeReport):
+class DataQualityReport(Report):
     def create_report(self, df_ref, df_prod):
-        pass
+        st.write('Not implemented yet!')
+
+
+class DataDriftReport(Report):
+    def create_report(self, df_ref, df_prod):
+        st.write('Not implemented yet!')
+
+
+class TargetDriftReport(Report):
+    def create_report(self, df_ref, df_prod):
+        st.write('Not implemented yet!')
+
+
+class LifetimeExplorationReport(Report):
+    def create_report(self, df_ref, df_prod):
+        st.write('Not implemented yet!')
 
         
